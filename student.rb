0291623466c19bde9e3518643b07cd3515f14817
dazehinn <dazehinn@yahoo.fr>
2023-07-01 18:26:29 +0100

    Update student class

--- student.rb
+++ student.rb
@@ -1,12 +1,17 @@
 require './person'
 
 class Student < Person
-  def initialize(age, classroom, name, parent_permission)
+  def initialize(age, name, parent_permission, classroom: nil)
     super(age, name, parent_permission)
     @classroom = classroom
+    assign_student_to_classroom if classroom
   end
 
   def play_hooky
     '"¯\(ツ)/¯"'
+  end
+
+  def assign_student_to_classroom
+    classroom.add_student(self) unless classroom.students.include?(self)
   end
 end
