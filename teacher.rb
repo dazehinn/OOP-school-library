909363d71de562bc9cf4e7964822a58625c9aed6
dazehinn <dazehinn@yahoo.fr>
2023-07-01 22:13:27 +0100

    Update teacher class

--- teacher.rb
+++ teacher.rb
@@ -1,8 +1,10 @@
 require './person'
 
 class Teacher < Person
+  attr_reader :specialization
+
   def initialize(specialization, age, name, parent_permission: true)
-    super(age, name, parent_permission)
+    super(age, name, parent_permission: parent_permission)
     @specialization = specialization
   end
 
